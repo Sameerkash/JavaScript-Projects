dbc6a07bf0a313d921226831eeeb1aee1ef843f1
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2020-04-07 20:20:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
-const EventEmitter = require('events');
-const eventEmitter = new EventEmitter();
+// const EventEmitter = require('events');
+// const eventEmitter = new EventEmitter();
 
-eventEmitter.on('tutorial', () => {
-    console.log('tutorial has occured');
-});
+// eventEmitter.on('tutorial', () => {
+//     console.log('tutorial has occured');
+// });
 
 // eventEmitter.emit('tutorial');
 
@@ -21,12 +21,28 @@ let num2 = Math.floor((Math.random() * 10 + 1));
 
 let answer = num1 + num1 + num2;
 
-r1.question(`what is ${num1} + ${num2}?`,
-    (user) => {
+r1.question(`what is ${num1} + ${num2}?\n`,
+    (userInput) => {
 
-        console.log(user);
-    })
+        if (userInput.trim() == answer) {
+            r1.close();
+        } else {
+            r1.setPrompt('Incorrect response please try again\n');
+            r1.prompt();
+            r1.on('line', (userInput) => {
+                if (userInput.trim() == answer)
+                    r1.close();
+                else {
+                    r1.setPrompt(`your answer of ${userInput} is Incorrect`);
+                    r1.prompt();
+                }
+            })
+        }
+    });
 
+r1.on('close', () => {
+    console.log("Correct!!");
+});
 
 
 
