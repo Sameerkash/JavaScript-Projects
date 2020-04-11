2f32aa4d785696509fbd71101a7b2f4ded97ff49
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2020-04-11 20:50:09 +0530

    added express js

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-const _ = require('lodash');
+// const _ = require('lodash');
 
-let example = _.fill([1, 2, 3, 4, 5], "banana", 1, 4);
-console.log(example);
+// let example = _.fill([1, 2, 3, 4, 5], "banana", 1, 4);
+// console.log(example);
+
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello world");
+});
+
+app.get('/example', (req, res) => [
+    res.send("hittng example route")
+]);
+
+app.get('/example/:name/:age', (req, res) => {
+    console.log(req.params);
+    res.send('example with rpute params');
+})
+
+app.listen(3000);
