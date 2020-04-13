de7d0732d3b1e06253527dc243622633c8b51033
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2020-04-13 17:42:58 +0530

    event loop

--- index.js
+++ index.js
@@ -4,20 +4,20 @@
 // console.log(example);
 
 
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello world");
-});
+// app.get('/', (req, res) => {
+//     res.send("Hello world");
+// });
 
-app.get('/example', (req, res) => [
-    res.send("hittng example route")
-]);
+// app.get('/example', (req, res) => [
+//     res.send("hittng example route")
+// ]);
 
-app.get('/example/:name/:age', (req, res) => {
-    console.log(req.params);
-    res.send('example with rpute params');
-})
+// app.get('/example/:name/:age', (req, res) => {
+//     console.log(req.params);
+//     res.send('example with rpute params');
+// })
 
-app.listen(3000);
+// app.listen(3000);
