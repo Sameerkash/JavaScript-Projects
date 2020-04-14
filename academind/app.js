96ba73d951aaee0284ad038c63ee211c1ccfcab3
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2020-04-14 18:03:28 +0530

    Express.js

--- academind/app.js
+++ academind/app.js
@@ -1,11 +1,23 @@
-const http = require('http');
 
 const express = require('express');
 
+const adminRoutes = require('./routes/admin');
+const shoproutes = require('./routes/shop');
+
+const bodyParser = require('body-parser');
+
 const app = express();
 
-const server = http.createServer(app);
+app.use(bodyParser.urlencoded({ extended: false }));
 
-server.listen(3000);
+app.use('/admin',adminRoutes);
+app.use(shoproutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1> Page Not Found</h1>');
+});
+
+
+app.listen(3000);
 
 
