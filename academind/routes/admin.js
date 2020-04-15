5117f184fa2924b7e005a19c7b06a7286c8aafd2
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2020-04-15 15:11:00 +0530

    added pug

--- academind/routes/admin.js
+++ academind/routes/admin.js
@@ -5,15 +5,16 @@ const router = express.Router();
 
 const products = [];
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    products.push({ title: req.body.title });
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-});
-
-router.post('/product', (req, res, next) => {
-    console.log(req.body);
+  });
+  
+  // /admin/add-product => POST
+  router.post('/add-product', (req, res, next) => {
+    products.push({ title: req.body.title });
     res.redirect('/');
-});
-
+  });
+  
 exports.routes = router;
 exports.products = products;
--- academind/routes/shop.js
+++ academind/routes/shop.js
@@ -5,8 +5,8 @@ const rootDir = require('../utils/path')
 const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
- 
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products;
+    res.render('shop', { prods: products, doctTitle: "Shop" });
 });
 
 module.exports = router;
