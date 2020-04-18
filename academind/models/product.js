1ae2e40105a2a316e4d9cc8b96ed92ea24d1d347
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2020-04-18 17:51:54 +0530

    enhcnaed app

--- academind/models/product.js
+++ academind/models/product.js
@@ -7,23 +7,26 @@ const p = path.join(
   'products.json'
 );
 
-const getProductsFromFile = (cb) => {
-  
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
-      return cb([]);
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    cb(JSON.parse(fileContent));
   });
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    getProductsFromFile((products) => {
+    getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
