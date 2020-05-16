6a93667f8152568ff13b9789dcc9a13729537311
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2020-05-16 20:32:46 +0530

    init

---
+++ shopping-list/src/index.js
@@ -0,0 +1,56 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+const url = require('url');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+  //insert menu
+  Menu.setApplicationMenu(mainMenuTemplate);
+
+};
+
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+
+const mainMenuTemplate = [
+  {
+    label: 'File'
+  }
+];
