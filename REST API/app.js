74224de6a89a37380a9059d7f393e2571a6fc8dc
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2020-06-04 16:52:23 +0530

    multer

--- REST API/app.js
+++ REST API/app.js
@@ -2,12 +2,32 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
+const multer = require('multer');
 
 const app = express();
 
+
+const fileStorage = multer.diskStorage({
+    destination: (req, fb, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, fb, cb) => {
+        cb(null, new Date().toISOString(+'-' + file.originalname));
+
+    }
+})
+
+const fileFilter = (req, fb, cb) => {
+    if (file.mimetype === 'images/png' || file.mimetype === 'images/jpeg' || file.mimetype === 'images/jpg') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
-
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
